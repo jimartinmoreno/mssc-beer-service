8c1a1291e9e9e4fc6b35b0db691ac0544a19299a
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2021-11-23 17:12:53 +0100

    Todos los projectos como modulos dentro de un workspace

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-11-23T12:10:46+0100",
+    date = "2021-11-23T16:50:17+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
